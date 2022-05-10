ee6bda30d861d61ce3ea6966ab313eed9bf60d0d
sharon alexa acevedo <alexa_acevedo2@hotmail.com>
2022-05-10 16:33:52 -0400

    delete method and links

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,6 @@ Rails.application.routes.draw do
   get "/tasks/:id/edit", to: "tasks#edit", as: :edit_task
   patch "/tasks/:id", to: "tasks#update"
 
-  # delete "/tasks/:id", to: "tasks#destroy"
+  delete "/tasks/:id", to: "tasks#destroy"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
